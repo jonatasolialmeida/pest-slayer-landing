3ae3ecdadb09a75710dd2f7993587cd0467e7eec
jonatas almeida <62767264+jonatasolialmeida@users.noreply.github.com>
2025-03-10 16:02:55 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -34,10 +34,6 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

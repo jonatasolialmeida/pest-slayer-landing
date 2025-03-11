dd6d7704cfd8e0f033a940c8aefd3b22dc603cb0
jonatas almeida <62767264+jonatasolialmeida@users.noreply.github.com>
2025-03-11 11:31:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,54 +1,22 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import path from "path";
-// import { componentTagger } from "lovable-tagger";
-
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   base: "/", // <-- Adicione esta linha
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-//   plugins: [
-//     react(),
-//     mode === "development" && componentTagger(),
-//   ].filter(Boolean),
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// }));
-
 import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-import react from "@vitejs/plugin-react-swc";
-
-import path from "path";
-
-export default defineConfig(({ mode }) => ({
-  base: "/", // <-- Isso garante que os caminhos relativos funcionem corretamente no Render
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    target: "esnext", // Ou "modules" se necessário
-    outDir: "dist",
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        format: "es", // Garante que os arquivos JS sejam tratados como módulos
-        entryFileNames: "assets/[name]-[hash].js",
-        chunkFileNames: "assets/[name]-[hash].js",
-        assetFileNames: "assets/[name]-[hash][extname]",
-      },
-    },
-  },
-}));
+ import react from "@vitejs/plugin-react-swc";
+ import path from "path";
+ import { componentTagger } from "lovable-tagger";
+ 
+ // https://vitejs.dev/config/
+ export default defineConfig(({ mode }) => ({
+   server: {
+     host: "::",
+     port: 8080,
+   },
+   plugins: [
+     react(),
+     mode === 'development' &&
+     componentTagger(),
+   ].filter(Boolean),
+   resolve: {
+     alias: {
+       "@": path.resolve(__dirname, "./src"),
+     },
+   },
+ }));
